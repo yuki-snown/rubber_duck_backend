be7325a7ac173d0ccb6dd5ae610a40acbb54a76a
yuki ohishi <yukiohishi@yukinoMacBook-Air.local>
2021-03-20 15:01:29 +0900

    ホストのホワイトリストtypo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.host << "ec2-3-112-68-32.ap-northeast-1.compute.amazonaws.com"
+  config.hosts << "ec2-3-112-68-32.ap-northeast-1.compute.amazonaws.com"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
