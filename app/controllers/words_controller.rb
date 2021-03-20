d9a4b5e78b5a50e4387e8bec4d4b543b803f6f6b
yuki oishi <oishi.yuki@moneyforward.co.jp>
2021-03-21 06:11:12 +0900

    分からない時のバリエーションを増やした

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -55,12 +55,19 @@ class WordsController < ApplicationController
             if msg.present? then
                 render json: {'msg': "#{msg.message}"}, :status => 200                
             else
-                if surface.length < 3 then
+                rnd = SecureRandom.random_number(4)
+                if surface.length < 4 then
                     render json: {'msg': "だよね！"}, :status => 200
-                elsif
-                    render json: {'msg': "ごめんよ！分かんないや\nもう少し教えて！ありがとう"}, :status => 200
+                elsif rnd = 0
+                    render json: {'msg': "ごめんよ！分かんないや"}, :status => 200
+                elsif rnd = 1
+                    render json: {'msg': "もう少し教えて！"}, :status => 200
+                elsif rnd = 2
+                    render json: {'msg': "他には？？"}, :status => 200
+                elsif rnd = 3
+                    render json: {'msg': "気分転換でもしたら？"}, :status => 200
                 end
-            end    
+            end
         end
     end
 end
